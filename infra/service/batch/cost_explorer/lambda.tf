45253d5e516944a7d46f79237d6b13ad7fc3209c
Masato Takeuchi <tamaco489@gmail.com>
2024-12-31 12:12:32 +0900

    feature cost explorer lambda function (#2)
    * add go modules
    
    * add Dockerfile and build cmd
    
    * add base.mk for terraform
    
    * add terraform exec env
    
    * make ecr
    
    * make common lambda logging iam policy
    
    * fix batch service name
    
    * make lambda function

---
+++ infra/service/batch/cost_explorer/lambda.tf
@@ -0,0 +1,24 @@
+resource "aws_lambda_function" "cost_explorer" {
+  function_name = local.fqn
+  description   = "Cost aggregation batch using AWS Cost Explorer"
+  role          = aws_iam_role.cost_explorer.arn
+  package_type  = "Image"
+  image_uri     = "${data.terraform_remote_state.ecr.outputs.cost_explorer.url}:cost_explorer_v0.0.0"
+  timeout       = 20
+  memory_size   = 128
+
+  lifecycle {
+    ignore_changes = [image_uri]
+  }
+
+  environment {
+    variables = {
+      SERVICE_NAME = "cost-explorer"
+      API_ENV      = "dev"
+    }
+  }
+
+  tags = {
+    Name = "${local.fqn}"
+  }
+}
