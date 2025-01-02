4349c7390a08c556d7a50641667af7ef128f721c
tamaco489 <tamaco489@gmail.com>
2025-01-02 22:06:44 +0900

    fix batch get secret manager

--- batch/internal/configuration/aws.go
+++ batch/internal/configuration/aws.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 )
 
 func loadAWSConf(ctx context.Context) error {
@@ -17,19 +15,4 @@ func loadAWSConf(ctx context.Context) error {
 	}
 	globalConfig.AWSConfig = cfg
 	return nil
-}
-
-func getFromSecretsManager(ctx context.Context, awsConfig aws.Config, secretIdList []string) (*secretsmanager.BatchGetSecretValueOutput, error) {
-
-	svc := secretsmanager.NewFromConfig(awsConfig)
-	input := &secretsmanager.BatchGetSecretValueInput{
-		SecretIdList: secretIdList,
-	}
-
-	result, err := svc.BatchGetSecretValue(ctx, input)
-	if err != nil {
-		return nil, fmt.Errorf("failed to batch-get secrets: %w", err)
-	}
-
-	return result, nil
 }
--- batch/internal/configuration/configuration.go
+++ batch/internal/configuration/configuration.go
@@ -2,35 +2,11 @@ package configuration
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"log/slog"
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/kelseyhightower/envconfig"
 )
-
-const (
-	baseSecretName          = "cost-explorer"
-	exchangeRatesSecretName = "exchange-rates/app-id"
-	slackConfigSecretName   = "slack/config"
-)
-
-var secretIdList []string
-
-func init() {
-	env := globalConfig.Env
-	secretIdList = []string{
-		generateSecretName(env, exchangeRatesSecretName),
-		generateSecretName(env, slackConfigSecretName),
-	}
-}
-
-func generateSecretName(env, secretName string) string {
-	secret := fmt.Sprintf("%s/%s/%s", baseSecretName, env, secretName)
-	return secret
-}
 
 type Config struct {
 	Env         string `envconfig:"ENV" default:"dev"`
@@ -62,51 +38,9 @@ func Load(ctx context.Context) (Config, error) {
 		return globalConfig, err
 	}
 
-	if err := loadSecrets(ctx, globalConfig.AWSConfig, globalConfig.Env); err != nil {
+	if err := loadSecrets(ctx, globalConfig); err != nil {
 		return globalConfig, err
 	}
 
 	return globalConfig, nil
-}
-
-func loadSecrets(ctx context.Context, awsConfig aws.Config, env string) error {
-	result, err := getFromSecretsManager(ctx, awsConfig, secretIdList)
-	if err != nil {
-		return err
-	}
-
-	for _, secret := range result.SecretValues {
-		switch *secret.Name {
-		case generateSecretName(env, slackConfigSecretName):
-			if err := parseAndSetSlackConfig(secret.SecretString); err != nil {
-				return err
-			}
-
-		case generateSecretName(env, exchangeRatesSecretName):
-			globalConfig.ExchangeRates.AppID = *secret.SecretString
-
-		default:
-			slog.WarnContext(ctx, "not found secret name",
-				slog.String("env", env),
-				slog.String("secret name", *secret.Name),
-			)
-		}
-	}
-
-	return nil
-}
-
-func parseAndSetSlackConfig(secretString *string) error {
-	var slackConfig struct {
-		DailyWebHookURL  string `json:"daily_webhook_url"`
-		WeeklyWebHookURL string `json:"weekly_webhook_url"`
-	}
-	if err := json.Unmarshal([]byte(*secretString), &slackConfig); err != nil {
-		return fmt.Errorf("failed to parse slack config: %w", err)
-	}
-
-	globalConfig.Slack.DailyWebHookURL = slackConfig.DailyWebHookURL
-	globalConfig.Slack.WeeklyWebHookURL = slackConfig.WeeklyWebHookURL
-
-	return nil
 }
