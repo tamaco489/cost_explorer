05ff91da4ee7d44f297ce33c446b69126afb3691
tamaco489 <tamaco489@gmail.com>
2025-01-02 17:26:01 +0900

    fix calk lib

---
+++ batch/internal/library/calc/calc.go
@@ -0,0 +1,9 @@
+package calc
+
+import "math"
+
+// RoundUpToTwoDecimalPlaces: float64 の値を小数点以下2桁で切り上げる。
+func RoundUpToTwoDecimalPlaces(value float64) float64 {
+	factor := math.Pow(10, 2)
+	return math.Ceil(value*factor) / factor
+}
